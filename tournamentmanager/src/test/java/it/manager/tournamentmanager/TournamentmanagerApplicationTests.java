59a26676021dfdd263669fabaa2764f9677c8f3c
Elias Pinna <157144311+PinnaElias@users.noreply.github.com>
2024-06-11 12:19:33 +0200

    Add files via upload

---
+++ tournamentmanager/src/test/java/it/manager/tournamentmanager/TournamentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.manager.tournamentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
