e446e6ab4bce010cfb10bf32739357bf8a056009
eliaspinna <eliaspinna99@gmail.com>
2024-06-17 14:21:45 +0200

    implementati tutte le repo

--- tournamentmanager/src/main/java/it/manager/tournamentmanager/entities/Match.java
+++ tournamentmanager/src/main/java/it/manager/tournamentmanager/entities/Match.java
@@ -6,7 +6,6 @@ import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.UUID;
 
 @Data
 @Entity
@@ -14,7 +13,7 @@ public class Match {
 
     @Id
     @GeneratedValue
-    private UUID id;
+    private int id;
     private LocalDate startingDate;
     private LocalTime startingTime;
 
---
+++ tournamentmanager/src/main/java/it/manager/tournamentmanager/repositories/TeamRepository.java
@@ -0,0 +1,16 @@
+package it.manager.tournamentmanager.repositories;
+
+import it.manager.tournamentmanager.entities.Game;
+import it.manager.tournamentmanager.entities.Team;
+import it.manager.tournamentmanager.entities.Tournament;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, UUID> {
+    List<Team> findAllByGame(Game game);
+
+}
